cec3c7020a416c8bde0542f8aeb5627deec9a917
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2023-01-18 16:11:03 -0300

    Criando o projeto do client

---
+++ clients/clientMicroServer/src/main/java/br/com/bruno/toshiaki/clientmicroserver/ClientMicroServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.bruno.toshiaki.clientmicroserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientMicroServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ClientMicroServerApplication.class, args);
+  }
+
+}
---
+++ clients/clientMicroServer/src/test/java/br/com/bruno/toshiaki/clientmicroserver/ClientMicroServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bruno.toshiaki.clientmicroserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientMicroServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
