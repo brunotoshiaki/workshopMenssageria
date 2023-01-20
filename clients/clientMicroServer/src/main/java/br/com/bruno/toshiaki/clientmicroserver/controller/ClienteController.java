1637a4b80c66e54bba242ddd09fccc5805ed3432
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2023-01-20 09:52:17 -0300

    Adicionando logs

--- clients/clientMicroServer/src/main/java/br/com/bruno/toshiaki/clientmicroserver/controller/ClienteController.java
+++ clients/clientMicroServer/src/main/java/br/com/bruno/toshiaki/clientmicroserver/controller/ClienteController.java
@@ -4,6 +4,7 @@ import br.com.bruno.toshiaki.clientmicroserver.controller.mapper.ClienteMapper;
 import br.com.bruno.toshiaki.clientmicroserver.controller.request.ClienteSaveRequest;
 import br.com.bruno.toshiaki.clientmicroserver.service.ClienteService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,17 +17,19 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @RestController
 @RequestMapping("/clientes")
 @RequiredArgsConstructor
+@Slf4j
 public class ClienteController {
 
   private final ClienteService service;
 
   @GetMapping
   public String status() {
+    log.info("Obetendo o status do microservice de clientes");
+
     return "ok";
   }
 
   @PostMapping
-
   public ResponseEntity save(@RequestBody final ClienteSaveRequest request) {
     final var cliente = ClienteMapper.INSTANCE.toEntity(request);
     this.service.save(cliente);
